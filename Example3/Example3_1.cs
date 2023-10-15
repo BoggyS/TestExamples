acb21f345a0ffb95e7b971e90947c5e8aec8b6c5
Bohuslav Chovhanskyi <bohuslav.chovhanskyi@gmail.com>
2023-10-15 16:37:19 +0300

    Example 3 additional improvements

--- Example3/Example3_1.cs
+++ Example3/Example3_1.cs
@@ -1,7 +1,7 @@
 public class Player
 {
-    private List<Vector2> activeWalkPath = null;
-    private bool isMoving => activeWalkPath != null;
+    private List<Vector2> activeWalkPath = new List<Vector2>();
+    private bool isMoving => activeWalkPath.Count > 0;
     private Player currentEnemy;
     private Vector2 currentPosition;
 
@@ -14,14 +14,14 @@ public class Player
     {
         if (currentEnemy == null)
         {
-            activeWalkPath = null;
+            activeWalkPath.Clear();
             return;
         }
 
-        activeWalkPath = TryBuildPathToCoord(currentEnemy.currentPosition);
+        TryBuildPathToCoordNonAlloc(currentEnemy.currentPosition, ref activeWalkPath);
     }
 
-    private List<Vector2> TryBuildPathToCoord(Vector2 target)
+    private bool TryBuildPathToCoordNonAlloc(Vector2 target, ref List<Vector2> path)
     {
         return < много строк кода по созданию пути из currentPosition в target >;
     }
